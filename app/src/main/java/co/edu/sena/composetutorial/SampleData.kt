327c0eab83e7e9af0316a443325c019bc6debc28
HernandezZuly <zulballes023@gmail.com>
2023-08-05 16:59:48 -0500

    Add examples files VHDL

---
+++ app/src/main/java/co/edu/sena/composetutorial/SampleData.kt
@@ -0,0 +1,65 @@
+package co.edu.sena.composetutorial
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Zuly",
+            "Hola, ahora si te puedo contestar las preguntas."
+        ),
+        Message(
+            "Valentina",
+            "Holaa!, mira estas son las preguntas:\n" +
+                    "Nombre Completo\n" +
+                    "Fecha de nacimiento\n" +
+                    "Ciudad en donde naciste\n" +
+                    "Edad Actual\n" +
+                    "Estado civil\n" +
+                    "Ciudad donde quisieras vivir\n"
+        ),
+        Message(
+            "Zuly",
+            "Respuestas:\n" +
+                    "Zuly Ballesteros\n" +
+                    "12/03/2000\n"+
+                    "Bogota\n" +
+                    "23 años\n"+
+                    "Soltera\n" +
+                    "Estados Unidos\n"
+        ),
+        Message(
+            "Valentina",
+            "Ok zuly, muchas gracias por contestar a las preguntas enviadas.\n" +
+                    "Te parece si te acercas mañana a la oficina para hablar personalmente y poderte dar la información completa sobre el trabajo?."
+        ),
+        Message(
+            "Zuly",
+            "Si señora, a que hora podria acercarme?."
+        ),
+        Message(
+            "Valentina",
+            "Te parece bien a las 7 de la mañana?."
+        ),
+        Message(
+            "Zuly",
+            "Si señora."
+        ),
+        Message(
+            "Valentina",
+            "Ok Zuly entonces nos vemos mañana a las 7 de la mañana en la oficina 108."
+        ),
+        Message(
+            "Zuly",
+            "Bueno si señora."
+        ),
+        Message(
+            "Valentina",
+            "Bueno Zuly, muchas gracias por contestar nuestras preguntas.\n" +
+                    "Que tengas un feliz resto de día."
+        ),
+        Message(
+            "Zuly",
+            "Igualmente."
+        ),
+    )
+}
---
+++ app/src/main/java/co/edu/sena/composetutorial/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package co.edu.sena.composetutorial.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple = Color(0xFFBF90D4)
+val PurpleLight = Color(0xFFF3A7F6)
+val PurpleDark = Color(0xFF9A90D4)
+
+val Blue = Color(0xFF8A84EA)
+val BlueLight = Color(0xFF7DB5F6)
+val BlueDark = Color(0xFF6C81D4)
